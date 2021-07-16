024ffba0e0fde400729e59b6fd8571424752ccfa
CS4Usama <cs.cybernetic@gmail.com>
2021-07-17 00:41:49 +0500

    UCC - GCUF FYP

---
+++ src/store/RootReducer.jsx
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+// import AuthReducer from './reducers/AuthReducer';
+
+const RootReducer = combineReducers({
+    // auth: AuthReducer,
+    // AuthReducer,
+});
+
+export default RootReducer;
